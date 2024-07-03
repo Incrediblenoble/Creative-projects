074b4278273cd23c25dc0be56ee102688e824531
Incrediblenoble <Incrediblenoble>
2024-07-03 12:50:51 +0000

    blutech

---
+++ src/pages/Department.tsx
@@ -0,0 +1,354 @@
+import { FunctionComponent } from "react";
+import "./Department.css";
+
+const Department: FunctionComponent = () => {
+  return (
+    <div className="department">
+      <div className="department-child" />
+      <div className="department-inner">
+        <div className="group-parent">
+          <img className="group-child" alt="" src="/group-1000004238@2x.png" />
+          <div className="unlimi">
+            <span>Unlimi</span>
+            <span className="span">.</span>
+          </div>
+        </div>
+      </div>
+      <div className="department-item" />
+      <div className="rectangle-div" />
+      <div className="div">
+        <div className="mask-group-parent">
+          <img className="mask-group-icon" alt="" src="/mask-group@2x.png" />
+          <div className="deko">Deko</div>
+          <img className="vector-icon" alt="" src="/vector.svg" />
+        </div>
+        <img className="div-child" alt="" src="/group-1000004248.svg" />
+      </div>
+      <div className="div1">
+        <div className="div-item" />
+        <div className="search-by-patients-parent">
+          <div className="search-by-patients">Search by patients...</div>
+          <img className="shape-icon" alt="" src="/shape.svg" />
+        </div>
+      </div>
+      <div className="div2">
+        <div className="div-inner">
+          <div className="frame-child" />
+        </div>
+        <div className="department-list">Department List</div>
+        <div className="rectangle-parent">
+          <div className="frame-item" />
+          <div className="sn-parent">
+            <div className="sn">S/N</div>
+            <div className="image">Image</div>
+            <div className="sku">SKU</div>
+            <div className="name">Name</div>
+            <div className="title">Title</div>
+            <div className="description">Description</div>
+            <div className="wages">Cost Price</div>
+            <div className="wages1">Quanity</div>
+            <div className="size">Size</div>
+            <div className="frame-inner" />
+          </div>
+          <div className="brand">Brand</div>
+        </div>
+        <div className="frame-div">
+          <div className="card-parent">
+            <div className="card">
+              <img
+                className="component-36-icon"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent">
+                <div className="parent">
+                  <div className="div3">1.</div>
+                  <img
+                    className="frame-icon"
+                    alt=""
+                    src="/frame-1171274981@2x.png"
+                  />
+                  <div className="mg234567">MG234567</div>
+                  <div className="gloss">Gloss</div>
+                  <div className="beauty-and-glamour">Beauty and glamour</div>
+                  <div className="lorem-ipsum-dolor">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div4">38.00</div>
+                  <div className="div5">36</div>
+                  <div className="div6">1,800</div>
+                  <div className="div7">18.00</div>
+                  <div className="frame-child1" />
+                </div>
+                <div className="group-item" />
+              </div>
+            </div>
+            <div className="card1">
+              <img
+                className="component-36-icon1"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-group">
+                <div className="group">
+                  <div className="div8">1.</div>
+                  <img
+                    className="frame-child2"
+                    alt=""
+                    src="/frame-1171274981@2x.png"
+                  />
+                  <div className="mg2345671">MG234567</div>
+                  <div className="gloss1">Gloss</div>
+                  <div className="beauty-and-glamour1">Beauty and glamour</div>
+                  <div className="lorem-ipsum-dolor1">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div9">38.00</div>
+                  <div className="div10">36</div>
+                  <div className="div11">1,800</div>
+                  <div className="div12">18.00</div>
+                  <div className="frame-child3" />
+                </div>
+                <div className="group-inner" />
+              </div>
+            </div>
+            <div className="card2">
+              <img
+                className="component-36-icon2"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-container">
+                <div className="container">
+                  <div className="div13">3.</div>
+                  <img
+                    className="frame-child4"
+                    alt=""
+                    src="/frame-1171274981-2@2x.png"
+                  />
+                  <div className="mg2345672">MG234567</div>
+                  <div className="gloss2">Gloss</div>
+                  <div className="gadget">Gadget</div>
+                  <div className="lorem-ipsum-dolor2">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div14">38.00</div>
+                  <div className="div15">36</div>
+                  <div className="div16">1,800</div>
+                  <div className="div17">18.00</div>
+                  <div className="frame-child5" />
+                </div>
+                <div className="group-child1" />
+              </div>
+            </div>
+            <div className="card3">
+              <img
+                className="component-36-icon3"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="group-div">
+                <div className="parent1">
+                  <div className="div18">4.</div>
+                  <img
+                    className="frame-child6"
+                    alt=""
+                    src="/frame-1171274981-3@2x.png"
+                  />
+                  <div className="mg2345673">MG234567</div>
+                  <div className="gloss3">Gloss</div>
+                  <div className="shoe">Shoe</div>
+                  <div className="lorem-ipsum-dolor3">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div19">38.00</div>
+                  <div className="div20">36</div>
+                  <div className="div21">1,800</div>
+                  <div className="div22">18.00</div>
+                  <div className="frame-child7" />
+                </div>
+                <div className="group-child2" />
+              </div>
+            </div>
+            <div className="card4">
+              <img
+                className="component-36-icon4"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent1">
+                <div className="parent2">
+                  <div className="div23">5.</div>
+                  <img
+                    className="frame-child8"
+                    alt=""
+                    src="/frame-1171274981-4@2x.png"
+                  />
+                  <div className="mg2345674">MG234567</div>
+                  <div className="gloss4">Gloss</div>
+                  <div className="cream">Cream</div>
+                  <div className="lorem-ipsum-dolor4">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div24">38.00</div>
+                  <div className="div25">36</div>
+                  <div className="div26">1,800</div>
+                  <div className="div27">18.00</div>
+                  <div className="frame-child9" />
+                </div>
+                <div className="group-child3" />
+              </div>
+            </div>
+            <div className="card5">
+              <img
+                className="component-36-icon5"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent2">
+                <div className="parent3">
+                  <div className="div28">6.</div>
+                  <img
+                    className="frame-child10"
+                    alt=""
+                    src="/frame-1171274981-5@2x.png"
+                  />
+                  <div className="mg2345675">MG234567</div>
+                  <div className="gloss5">Gloss</div>
+                  <div className="lotions">Lotions</div>
+                  <div className="lorem-ipsum-dolor5">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div29">38.00</div>
+                  <div className="div30">36</div>
+                  <div className="div31">1,800</div>
+                  <div className="div32">18.00</div>
+                  <div className="frame-child11" />
+                </div>
+                <div className="group-child4" />
+              </div>
+            </div>
+            <div className="card6">
+              <img
+                className="component-36-icon6"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent3">
+                <div className="parent4">
+                  <div className="div33">7.</div>
+                  <img
+                    className="frame-child12"
+                    alt=""
+                    src="/frame-1171274981-6@2x.png"
+                  />
+                  <div className="mg2345676">MG234567</div>
+                  <div className="gloss6">Gloss</div>
+                  <div className="beauty-and-glamour2">Beauty and glamour</div>
+                  <div className="lorem-ipsum-dolor6">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div34">38.00</div>
+                  <div className="div35">36</div>
+                  <div className="div36">1,800</div>
+                  <div className="div37">18.00</div>
+                  <div className="frame-child13" />
+                </div>
+                <div className="group-child5" />
+              </div>
+            </div>
+            <div className="card7">
+              <img
+                className="component-36-icon7"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent4">
+                <div className="parent5">
+                  <div className="div38">8.</div>
+                  <img
+                    className="frame-child14"
+                    alt=""
+                    src="/frame-1171274981-7@2x.png"
+                  />
+                  <div className="mg2345677">MG234567</div>
+                  <div className="gloss7">Gloss</div>
+                  <div className="watch">Watch</div>
+                  <div className="lorem-ipsum-dolor7">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div39">38.00</div>
+                  <div className="div40">36</div>
+                  <div className="div41">1,800</div>
+                  <div className="div42">18.00</div>
+                  <div className="frame-child15" />
+                </div>
+                <div className="group-child6" />
+              </div>
+            </div>
+            <div className="card8">
+              <img
+                className="component-36-icon8"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent5">
+                <div className="parent6">
+                  <div className="div43">9.</div>
+                  <img
+                    className="frame-child16"
+                    alt=""
+                    src="/frame-1171274981-8@2x.png"
+                  />
+                  <div className="mg2345678">MG234567</div>
+                  <div className="gloss8">Gloss</div>
+                  <div className="beauty-and-glamour3">Beauty and glamour</div>
+                  <div className="lorem-ipsum-dolor8">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div44">38.00</div>
+                  <div className="div45">36</div>
+                  <div className="div46">1,800</div>
+                  <div className="div47">18.00</div>
+                  <div className="frame-child17" />
+                </div>
+                <div className="group-child7" />
+              </div>
+            </div>
+            <div className="card9">
+              <img
+                className="component-36-icon9"
+                alt=""
+                src="/component-36.svg"
+              />
+              <div className="frame-parent6">
+                <div className="parent7">
+                  <div className="div48">10.</div>
+                  <img
+                    className="frame-child18"
+                    alt=""
+                    src="/frame-1171274981-9@2x.png"
+                  />
+                  <div className="mg2345679">MG234567</div>
+                  <div className="gloss9">Gloss</div>
+                  <div className="beauty-and-glamour4">Beauty and glamour</div>
+                  <div className="lorem-ipsum-dolor9">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </div>
+                  <div className="div49">38.00</div>
+                  <div className="div50">36</div>
+                  <div className="div51">1,800</div>
+                  <div className="div52">18.00</div>
+                  <div className="frame-child19" />
+                </div>
+                <div className="group-child8" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Department;
